2a1e030cc0e2f8da3d922eb2f526fb0e6dcdf727
saalik-Makhdoomi <makhdoomisaalik@gmail.com>
2024-09-05 13:04:03 +0530

    JavaScript

---
+++ script.js
@@ -0,0 +1,37 @@
+const accessKey = "_bJclOUllSN5KrFKrb0LS_pVRppi3hSqJLGOkd9ga1s"
+
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("Search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-button")
+
+let inputData = ""
+let page = 1;
+
+async function searchImages(){
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    const results = data.results
+
+    if(page===1){
+        searchResults.innerHTML = ""
+    }
+
+    results.map((result) =>{
+        const imageWrapper = document.createElement('div')
+        imageWrapper.classList.add("search-result")
+        const image = document.createElement('img')
+        image.src = result.urls.small
+        image.alt = result.alt_description
+        const imageLink = document.createElement('a')
+        imageLink.href = result.links.html
+        imageLink.target = "_blank"
+        imageLink.textContent = result.alt_description
+
+        
+     })
+}
