634efaee48a8bd5fa101bbbb1a605e7c2ac34520
saalik-Makhdoomi <makhdoomisaalik@gmail.com>
2024-09-05 13:23:37 +0530

    javaScript

--- script.js
+++ script.js
@@ -22,16 +22,35 @@ async function searchImages(){
     }
 
     results.map((result) =>{
-        const imageWrapper = document.createElement('div')
-        imageWrapper.classList.add("search-result")
-        const image = document.createElement('img')
-        image.src = result.urls.small
-        image.alt = result.alt_description
-        const imageLink = document.createElement('a')
-        imageLink.href = result.links.html
-        imageLink.target = "_blank"
-        imageLink.textContent = result.alt_description
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
 
-        
-     })
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+     });
+
+     page++
+     if(page > 1){
+        showMore.style.display = "block"
+     }
 }
+
+
+formEl.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    page = 1;
+    searchImages()
+});
+
+showMore.addEventListener("click", () =>{
+  
+    searchImages()
+});
